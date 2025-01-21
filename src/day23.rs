9079ea234665e4d1bcdfbfbf2dab0f8dc9f5350b
Milan Justel <milancjustel@gmail.com>
2025-01-20 20:31:25 -0800

    Complete day 23 part 2

--- src/day23.rs
+++ src/day23.rs
@@ -8,8 +8,10 @@ pub fn run() {
 
 fn part_1(contents: &str) -> usize {
     let connections = parse_input(contents);
+    let graph = build_graph(&connections);
+    let triples = find_triples(&graph);
 
-    count_triples_with_t(&connections)
+    count_triples_with_t(&triples)
 }
 
 fn parse_input(contents: &str) -> Vec<(&str, &str)> {
@@ -22,10 +24,8 @@ fn parse_input(contents: &str) -> Vec<(&str, &str)> {
         .collect()
 }
 
-fn count_triples_with_t(connections: &[(&str, &str)]) -> usize {
+fn build_graph<'x>(connections: &[(&'x str, &'x str)]) -> HashMap<&'x str, HashSet<&'x str>> {
     let mut graph: HashMap<&str, HashSet<&str>> = HashMap::new();
-
-    let mut triples = HashSet::new();
 
     for (a, b) in connections {
         graph
@@ -42,7 +42,13 @@ fn count_triples_with_t(connections: &[(&str, &str)]) -> usize {
             .or_insert(HashSet::from_iter([*a]));
     }
 
-    for (first, values) in &graph {
+    graph
+}
+
+fn find_triples<'x>(graph: &HashMap<&'x str, HashSet<&'x str>>) -> HashSet<Vec<&'x str>> {
+    let mut triples = HashSet::new();
+
+    for (first, values) in graph {
         // Go through values to see if they have each other.
         for second in values {
             let possible_thirds = graph.get(second).unwrap();
@@ -53,27 +59,77 @@ fn count_triples_with_t(connections: &[(&str, &str)]) -> usize {
                 }
 
                 if possible_thirds.contains(third) {
-                    triples.insert(sort_nodes(first, second, third));
+                    let mut triple = vec![*first, *second, *third];
+                    triple.sort();
+
+                    triples.insert(triple);
                 }
             }
         }
     }
 
     triples
+}
+
+fn count_triples_with_t(triples: &HashSet<Vec<&str>>) -> usize {
+    triples
         .iter()
-        .filter(|(a, b, c)| a.starts_with('t') || b.starts_with('t') || c.starts_with('t'))
+        .filter(|triple| triple.iter().any(|item| item.starts_with('t')))
         .count()
 }
 
-fn sort_nodes<'x>(a: &'x str, b: &'x str, c: &'x str) -> (&'x str, &'x str, &'x str) {
-    let mut items = vec![a, b, c];
-    items.sort();
+fn part_2(contents: &str) -> String {
+    let connections = parse_input(contents);
+    let graph = build_graph(&connections);
+    let triples = find_triples(&graph);
 
-    (items[0], items[1], items[2])
+    find_biggest_group(&graph, &triples).join(",")
 }
 
-fn part_2(contents: &str) -> usize {
-    0
+fn find_biggest_group<'x>(
+    graph: &HashMap<&'x str, HashSet<&'x str>>,
+    triples: &HashSet<Vec<&'x str>>,
+) -> Vec<&'x str> {
+    let groups: Vec<HashSet<&str>> = triples
+        .iter()
+        .map(|triple| HashSet::from_iter(triple.iter().cloned()))
+        .collect();
+
+    groups
+        .iter()
+        .map(|group| expand_group(graph, group))
+        .max_by(|a, b| a.len().cmp(&b.len()))
+        .unwrap()
+}
+
+fn expand_group<'x>(
+    graph: &HashMap<&'x str, HashSet<&'x str>>,
+    group: &HashSet<&'x str>,
+) -> Vec<&'x str> {
+    let others = graph
+        .keys()
+        .filter(|&&key| !group.contains(key))
+        .collect::<HashSet<_>>();
+
+    let mut current_group = Vec::from_iter(group.iter().cloned());
+
+    for other in others.iter() {
+        let mut connected = true;
+        for item in &current_group {
+            if !graph.get(item).unwrap().contains(&**other) {
+                connected = false;
+                break;
+            }
+        }
+
+        if connected {
+            current_group.push(other);
+        }
+    }
+
+    current_group.sort();
+
+    current_group
 }
 
 #[cfg(test)]
@@ -98,13 +154,13 @@ mod tests {
     fn test_example_part_2() {
         let contents = utilities::read_file_data(DAY, "example.txt");
 
-        assert_eq!(part_2(&contents), 0);
+        assert_eq!(part_2(&contents), "co,de,ka,ta");
     }
 
     #[test]
     fn test_input_part_2() {
         let contents = utilities::read_file_data(DAY, "input.txt");
 
-        assert_eq!(part_2(&contents), 0);
+        assert_eq!(part_2(&contents), "ag,gh,hh,iv,jx,nq,oc,qm,rb,sm,vm,wu,zr");
     }
 }
